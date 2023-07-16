50b951c37015da1be9444480ae2bed3de1324a77
Bruno Pasquetti <brunopasquetti16@gmail.com>
2023-07-16 20:07:09 -0300

    introduzi no codigo um comando que salva a ultima senha gerada e quando rodar ele de novo ele pergunta se deseja ver as ultimas senhas geradas

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import random 
+import pickle
 vermelho = ("\033[1;31m" )
+branco = (255, 255, 255)
 print("Seja bem vindo ao gerador de senhas")
 
 caracteres = 'abcdefghijklmnopqrstuvwABCDEFGHIJKLMNOPQRSTUVW!@#$%¨&*().,?0123456789'
 
-numero = input ("Quantidades de senhas para gerar:")
-numero= int(numero)
+vizualizar_senha = input ("Deseja vizualizar suas senhas anteriores 1- Sim, 0- Não:")
+if vizualizar_senha =="1":
+    try:
+        with open ("senhas.pkl", "rb") as arquivo:
+            senhas_salvas = pickle.load (arquivo)
+        print("Aqui estão suas senhas anteriores:")
+        for senha_salva in senhas_salvas:
+            print(senha_salva)
+    except FileNotFoundError:
+        print('Nenhuma senha anterior econtrada.')
+    print("\n")
 
-tamanho = input ("Qual tamanho da sua senha:")
-tamanho = int(tamanho)
+numero = int(input ("Quantidades de senhas para gerar:"))
+tamanho = int(input ("Qual tamanho da sua senha:"))
 
 print("\nAqui estão suas senhas:")
 
-for pwd in range (numero):
-    senhas = ""
-    for c in range (tamanho):
-        senhas += random.choice(caracteres)
-    print(vermelho, senhas)
+senhas = []
+
+for _ in range (numero):
+    senha = "".join(random.choice(caracteres)for _ in range (tamanho))
+    senhas.append (senha)
+    print(senha)
+
+salvar = input("pressione Enter para salvar")
+
+with open ("senhas.pkl", "wb") as arquivo:
+    pickle.dump(senhas, arquivo)
+print("As senhas foram salvas com sucesso!")
