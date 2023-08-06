8f0d7783b3fc5b183c6481b1e116b96549af3435
Bruno Pasquetti <brunopasquetti16@gmail.com>
2023-08-06 17:18:06 -0300

    Adicionei um while, que nele consigo repetitir o programa e ver se o usuario deseja fazer mais senhas

--- main.py
+++ main.py
@@ -17,21 +17,29 @@ if vizualizar_senha =="1":
     except FileNotFoundError:
         print('Nenhuma senha anterior econtrada.')
     print("\n")
+while True:
+    gerar = int(input ("Deseja gerar uma senha: 1- sim, 0- não"))
+    if gerar == 0:
+            print("Muito obrigado por usar nosso programa, suas senhas estarão salvas!")
+            exit
+            break
+    elif gerar == 1:
+        numero = int(input ("Quantidades de senhas para gerar:"))
+        tamanho = int(input ("Qual tamanho da sua senha:"))
 
-numero = int(input ("Quantidades de senhas para gerar:"))
-tamanho = int(input ("Qual tamanho da sua senha:"))
+        print("\nAqui estão suas senhas:")
 
-print("\nAqui estão suas senhas:")
+        senhas = []
 
-senhas = []
+        for _ in range (numero):
+            senha = "".join(random.choice(caracteres)for _ in range (tamanho))
+            senhas.append (senha)
+            print(senha)
 
-for _ in range (numero):
-    senha = "".join(random.choice(caracteres)for _ in range (tamanho))
-    senhas.append (senha)
-    print(senha)
+        salvar = input("pressione Enter para salvar")
 
-salvar = input("pressione Enter para salvar")
-
-with open ("senhas.pkl", "wb") as arquivo:
-    pickle.dump(senhas, arquivo)
-print("As senhas foram salvas com sucesso!")
+        with open ("senhas.pkl", "wb") as arquivo:
+            pickle.dump(senhas, arquivo)
+        print("As senhas foram salvas com sucesso!")
+    else: 
+        print("Entrada inválida! Tente novamente.")
